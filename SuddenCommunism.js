6e2a646730773b4de63601eca548e95edb49da8c
KaizerIzanagi <134472157+KaizerIzanagi@users.noreply.github.com>
2023-08-29 13:35:45 +0800

    New Push
    E

---
+++ SuddenCommunism.js
@@ -0,0 +1,8 @@
+function SuddenCommunism() {
+  var x = document.getElementById("MySurprise");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
---
+++ new.js
@@ -0,0 +1,5 @@
+let h2 = document.querySelector(".external");
+h2.addEventListener("mouseenter", function (e) 
+{
+	h2.innerText = "Save The Fans!";
+});
